55f8b2a75e35438280cf1899adcda9cc0a1797ab
Aline Bianca <88475157+alinebianca2@users.noreply.github.com>
2023-09-30 23:55:56 -0300

    Time: 53 ms (84.58%), Space: 44.9 MB (22.97%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let num = init;
+    return {
+        increment: () => num+=1,
+        reset: () => num = init,
+        decrement: () => num-=1
+    };
+};
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
